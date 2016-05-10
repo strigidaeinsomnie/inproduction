5ba60b88240db2a592edf41a9ba7db78ea8a6cee
strigidaeinsomnie <strigidaeinsomnie@gmail.com>
2016-05-10 20:35:55 +0900

    "ffffu"

---
+++ angelfly/arduino/firmatatest.py
@@ -0,0 +1,11 @@
+from firmata import *
+
+arduino = Arduino("/dev/ttyACM0",baudrate=57600) # 3 for COM4
+arduino.pin_mode(13,firmata.OUTPUT)
+arduino.delay(2)
+
+while True:
+    arduino.digital_write(13,firmata.HIGH)
+    arduino.delay(1)
+    arduino.digital_write(13,firmata.LOW)
+    arduino.delay(1)
--- angelfly/arduino/serialtest.py
+++ angelfly/arduino/serialtest.py
@@ -3,7 +3,7 @@ import time
 
 ser = serial.Serial()
 ser.baudrate = 9600
-ser.port = 2 # COM3->2,COM5->4
+ser.port = "/dev/ttyACM0" # COM3->2,COM5->4
 ser.open()
 time.sleep(1)
 
