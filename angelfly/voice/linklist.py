df64b7489bb92db2b7afad65c56d11bb436b64eb
strigidaeinsomnie <strigidaeinsomnie@gmail.com>
2016-05-25 22:42:26 +0900

    movededed

--- angelfly/voice/linklist.py
+++ angelfly/voice/linklist.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 import re
-import urllib2.request
+import urllib2
 
 def linklist(arg1, arg2) :
-    html = urllib2.request.urlopen(arg1)
+    html = urllib2.urlopen(arg1)
     soup = BeautifulSoup(html, 'lxml')
     return soup.find_all(href=re.compile(arg2))
--- angelfly/voice/xhtmlopen.py
+++ angelfly/voice/xhtmlopen.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
-import urllib2.request
+import urllib2
 
 def xhtmlopen(arg1, arg2, arg3) :
-    html = urllib2.request.urlopen(arg1)
+    html = urllib2.urlopen(arg1)
     soup = BeautifulSoup(html, 'lxml')
     return soup.find(arg2,{arg3})
