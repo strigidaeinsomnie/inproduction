9deb97e1ad4086b5f2675412a9723760ca24d789
strigidaeinsomnie <strigidaeinsomnie@gmail.com>
2016-05-29 13:45:57 +0900

    in time

---
+++ angelfly/voice/voice-v3.py
@@ -0,0 +1,87 @@
+#-------------------------------------------------------
+
+from linklist import *
+from choicelink import *
+from fathermake import *
+from xhtmlopen import *
+from washxhtml import *
+from findsentence import *
+from dustshoot import *
+from lamemoire import *
+
+import time
+import re
+import serial
+
+#-------------------------------------------------------
+
+mother = 'http://www.aozora.gr.jp/'
+fatherfront = ''
+fatherrear = 'c'
+keyinitial = 'index_pages/sakuhin'
+keytitle = '../cards'
+keyxhtml = '.files/'
+keyxhtmlclass1 = 'div'
+keyxhtmlclass2 = 'main_text'
+
+initialfront = '"'
+initialrear = '"'
+titlefront = '/'
+titlerear = '"'
+xhtmlfront = '/'
+xhtmlrear = '"'
+
+linklists = None
+links = None
+linktitle = None
+linklistxhtml = None
+linkxhtml = None
+xhtml = None
+sentence = None
+word = None
+
+ser = serial.Serial(port = '/dev/ttyAMA0', baudrate = 9600)
+clear = '\r\r'
+
+#-------------------------------------------------------
+
+while :
+
+    while word == None :
+
+        while sentence == None :
+            linklists = linklist(mother, keyinitial)
+            links = choicelink(mother, linklists, initialfront, initialrear)
+
+            linklists = linklist(links, keytitle)
+            linktitle = choicelink(mother, linklists, titlefront, titlerear)
+
+            father = fathermake(linktitle, fatherfront, fatherrear)
+
+            linklistxhtml = linklist(linktitle, keyxhtml)
+
+
+            while xhtml == None:
+                linkxhtml = choicelink(father, linklistxhtml, xhtmlfront, xhtmlrear)
+
+                xhtml = xhtmlopen(linkxhtml, keyxhtmlclass1, keyxhtmlclass2)
+
+
+            washedxhtml = washxhtml(xhtml)
+
+            text = dustshoot(washedxhtml)
+
+            sentence = findsentence(text)
+
+        else :
+            word = lamemoire(sentence)
+
+    else :
+        ser.write(clear.encode())
+        ser.write(word.encode())
+        ser.write(clear.encode())
+
+        print(word)
+
+    time.sleep(5)
+#-------------------------------------------------------
