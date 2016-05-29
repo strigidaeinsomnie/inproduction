072231b5291c5706c34e535508f60e911f1636f3
strigidaeinsomnie <strigidaeinsomnie@gmail.com>
2016-05-29 13:35:47 +0900

    break

--- angelfly/voice/findsentence.py
+++ angelfly/voice/findsentence.py
@@ -1,9 +1,11 @@
 import random
 
 def findsentence(arg1) :
+    print(arg1)
+    
     if arg1 == None :
         return None
-        
+
     else :
         sentence = random.choice(arg1)
         return sentence
