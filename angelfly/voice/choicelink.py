576c073d053c27604df6d205c61aa058881e2da2
strigidaeinsomnie <strigidaeinsomnie@gmail.com>
2016-05-29 13:39:37 +0900

    break

--- angelfly/voice/choicelink.py
+++ angelfly/voice/choicelink.py
@@ -1,8 +1,14 @@
 import random
 
 def choicelink(arg1, arg2, arg3, arg4) :
-    prelink = str(random.choice(arg2))
-    front = prelink.find(arg3) + 1
-    rear = prelink.rfind(arg4)
-    slicedlink = prelink[front:rear]
-    return arg1 + slicedlink
+    print(arg2)
+
+    if arg2 == None :
+        return None
+
+    else :
+        prelink = str(random.choice(arg2))
+        front = prelink.find(arg3) + 1
+        rear = prelink.rfind(arg4)
+        slicedlink = prelink[front:rear]
+        return arg1 + slicedlink
