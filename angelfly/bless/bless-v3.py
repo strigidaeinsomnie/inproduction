aee278097f8eb9e332d47c82e75bbbdce64720eb
strigidaeinsomnie <strigidaeinsomnie@gmail.com>
2016-06-07 16:26:43 +0900

    m

--- angelfly/bless/bless-v3.py
+++ angelfly/bless/bless-v3.py
@@ -112,7 +112,7 @@ while sw1 <= 100 :
     print(text) #--ルビをとってかぎ括弧さがす
 
 #    try :
-    sentence = radom.choice(text)
+    sentence = random.choice(text)
 #    except IndexError :
 #        continue
 
